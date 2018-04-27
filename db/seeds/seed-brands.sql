6c55267c846411300f27113d4a521767672032d1
andy <thamandyy@gmail.com>
2018-04-27 14:54:32 -0400

    changed seeds to make more sense

--- db/seeds/seed-brands.sql
+++ db/seeds/seed-brands.sql
@@ -1,6 +1,6 @@
-INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('name1','location1','story1','quote1','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_4_zjw9fs.png',true);
-INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('name2','location2','story2','quote2','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_3_dwtanr.png',true);
-INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('name3','location3','story3','quote3','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_2_hdzwaa.png',true);
-INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('name4','location4','story4','quote4','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_1_il3fia.png',true);
-INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('name5','location5','story5','quote5','image5',false);
-INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('name6','location6','story6','quote6','image6',false);
+INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('brand1','location1','story1','quote1','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_4_zjw9fs.png',true);
+INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('brand2','location2','story2','quote2','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_3_dwtanr.png',true);
+INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('brand3','location3','story3','quote3','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_2_hdzwaa.png',true);
+INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('brand4','location4','story4','quote4','http://res.cloudinary.com/camcash17/image/upload/v1517597241/featured_brand_img_1_il3fia.png',true);
+INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('brand5','location5','story5','quote5','image5',false);
+INSERT INTO brands(name,location,story,quote,image,featured) VALUES ('brand6','location6','story6','quote6','image6',false);
--- db/seeds/seed-products.sql
+++ db/seeds/seed-products.sql
@@ -1,6 +1,6 @@
-INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name1','1','description1','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_4_zll5xg.png',true);
-INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name2',Null,'description2','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_2_nom6ov.png',true);
-INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name3',Null,'description3','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_3_buin5c.png',true);
-INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name4',Null,'description4','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_1_hkp1hg.png',true);
-INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name5',Null,'description5','image5',false);
-INSERT INTO products(name,brand_id,description,image,featured) VALUES ('name6',Null,'description6','image6',false);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('product name1','1','description1','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_4_zll5xg.png',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('product name2','1','description2','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_2_nom6ov.png',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('product name3','2','description3','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_3_buin5c.png',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('product name4','4','description4','http://res.cloudinary.com/camcash17/image/upload/v1517597139/featured_product_img_1_hkp1hg.png',true);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('product name5','3','description5','image5',false);
+INSERT INTO products(name,brand_id,description,image,featured) VALUES ('product name6','1','description6','image6',false);
